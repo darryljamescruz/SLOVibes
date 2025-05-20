d4dd86d2efecb612c61a42f0afc6b03c302e74de
Darryl James Cruz <darryljamescruz@gmail.com>
2025-05-19 23:27:33 -0700

    frontend scaffoled - need to structure

---
+++ packages/react-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="text-2xl font-din text-center p-4 m-4"> 
+      <p> inner machimations of my mind is an enigma this is App.jsx</p>
+    </div>
+
+  )
+}
+
+export default App
